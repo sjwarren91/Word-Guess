238fa246c0d299154ef49396bf63c74fa539aad9
Sam Warren <sjwarren91@gmail.com>
2019-07-01 14:42:30 +1000

    add new game button, add key listener, key check logic works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,42 @@
 var wordArray = ["pumpkin", "horse", "engineering"];
 var guessField = document.getElementById("guesses");
-
+var newGameButton = document.getElementById("new-game-button");
 var currentWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-var currentGuess = [];
-console.log(currentWord);
-console.log(currentWord.length)
+var guessArray = [];
 
-for (let i = 0; i < currentWord.length; i++){
-    currentGuess.push("_ ");
+console.log(currentWord);
+console.log(currentWord.length);
+
+function newGame() {
+  for (let i = 0; i < currentWord.length; i++) {
+    guessArray.push("_");
+  }
+  for (let i = 0; i < guessArray.length; i++) {
+    guessField.innerHTML += guessArray[i] + " ";
+  }
 }
 
-guessField.innerHTML = currentGuess;
+function arrayPrint(array) {
+  guessField.innerHTML = "";
+  for (let i = 0; i < guessArray.length; i++) {
+    guessField.innerHTML += guessArray[i] + " ";
+  }
+}
 
+newGameButton.addEventListener("click", function(){
+    guessField.innerHTML = "";
+    newGame();
+})
 
 document.onkeyup = function(event) {
-    var keyPress = event.key;
+  var keyPress = event.key;
 
-}
+  for (let i = 0; i < currentWord.length; i++) {
+    if (keyPress === currentWord[i]) {
+      guessArray[i] = keyPress;
+      console.log(guessArray);
+    }
+  }
+
+  arrayPrint(guessArray);
+};
